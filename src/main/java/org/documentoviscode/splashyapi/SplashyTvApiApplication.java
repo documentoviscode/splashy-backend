0c0c7f5d6d2ad0e5b9ebeb91117069db85a9bef9
Mixss <michalziemiec@wp.pl>
2023-10-20 12:12:13 +0200

    Added basic project files
    Signed-off-by: Mixss <michalziemiec@wp.pl>

---
+++ src/main/java/org/documentoviscode/splashyapi/SplashyTvApiApplication.java
@@ -0,0 +1,13 @@
+package org.documentoviscode.splashyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplashyTvApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplashyTvApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/documentoviscode/splashyapi/SplashyTvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.documentoviscode.splashyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplashyTvApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
