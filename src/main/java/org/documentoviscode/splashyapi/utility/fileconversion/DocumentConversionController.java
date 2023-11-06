5ab290e77a0226dbc7f2b256a4c38b59a53f32fb
Jakub Link <s184469@student.pg.edu.pl>
2023-11-06 11:05:35 +0100

    DC-68: dodanie draftu generowania raportu miesięcznego (bez uploadu)

--- src/main/java/org/documentoviscode/splashyapi/utility/fileconversion/DocumentConversionController.java
+++ src/main/java/org/documentoviscode/splashyapi/utility/fileconversion/DocumentConversionController.java
@@ -4,13 +4,22 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.documentoviscode.splashyapi.controllers.GoogleDriveController;
+import org.docx4j.dml.wordprocessingDrawing.Inline;
+import org.docx4j.jaxb.Context;
+import org.docx4j.model.table.TblFactory;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.BinaryPartAbstractImage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.*;
 import org.json.simple.JSONObject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -89,5 +98,122 @@ public class DocumentConversionController{
 
         new File(reportPath + reportFileName).delete();
         return reportFileName;
+    }
+
+    public String generateMonthlyReportDOCX(String fileId) throws Exception {
+        String reportPath = "src/main/resources/";
+        String downloadedFileName = "downloaded.json";
+
+        GoogleDriveController controller = new GoogleDriveController();
+        File downloaded = new File(reportPath + downloadedFileName);
+        try (FileOutputStream outputStream = new FileOutputStream(downloaded)) {
+            outputStream.write(controller.downloadFile(fileId).getBody());
+        }
+
+        Document jsonDoc = new Document();
+        jsonDoc.readFrom(reportPath + downloadedFileName);
+
+        Map<String, Object> map = Map.of(
+                "id", 0,
+                "type", "PDF",
+                "creationDate", "2023-11-05",
+                "users", List.of(Map.of("id", 0,
+                        "name", "string",
+                        "surname", "string",
+                        "email", "string",
+                        "role", "CLIENT",
+                        "avatar", "string",
+                        "documents", "String"
+                ), ""),
+                "startDate", "2023-11-05",
+                "endDate", "2023-11-05",
+                "viewers", 0,
+                "hoursWatched", 0,
+                "donations", 0,
+                "gdriveLink", "string");
+
+        JSONObject data = new JSONObject(map);
+//        downloaded.delete();
+
+        JSONObject partner = new JSONObject(((List<Map>) data.get("users")).get(0));
+        String reportFileName = "monthlyReport_" + partner.get("id") + "_" + data.get("creationDate") + ".docx";
+
+        WordprocessingMLPackage wordPackage = WordprocessingMLPackage.createPackage();
+        MainDocumentPart mainDocumentPart = wordPackage.getMainDocumentPart();
+
+        mainDocumentPart.addStyledParagraphOfText("Title", "Monthly Report");
+        mainDocumentPart.addParagraphOfText("Date from: " + data.get("startDate"));
+        mainDocumentPart.addParagraphOfText("Date to: " + data.get("endDate"));
+        mainDocumentPart.addParagraphOfText("");
+        mainDocumentPart.addParagraphOfText("Partner:");
+        mainDocumentPart.addParagraphOfText("Name: " + partner.get("name"));
+        mainDocumentPart.addParagraphOfText("Surname: " + partner.get("surname"));
+        mainDocumentPart.addParagraphOfText("E-mail: " + partner.get("email"));
+
+        ObjectFactory factory = Context.getWmlObjectFactory();
+        P p = factory.createP();
+        R r = factory.createR();
+        Text t = factory.createText();
+        r.getContent().add(t);
+        p.getContent().add(r);
+
+        int writableWidthTwips = wordPackage.getDocumentModel()
+                .getSections().get(0).getPageDimensions().getWritableWidthTwips();
+        int columnNumber = 2;
+        Tbl tbl = TblFactory.createTable(3, columnNumber, writableWidthTwips/columnNumber);
+        addCellContent(tbl, 0, 0, "Number of viewers");
+        addCellContent(tbl, 0, 1, data.get("viewers").toString());
+        addCellContent(tbl, 1, 0, "Number of hours watched");
+        addCellContent(tbl, 1, 1, data.get("hoursWatched").toString());
+        addCellContent(tbl, 2, 0, "Total donations");
+        addCellContent(tbl, 2, 1, data.get("donations") + " $");
+
+        mainDocumentPart.addParagraphOfText("");
+        mainDocumentPart.getContent().add(tbl);
+        mainDocumentPart.addParagraphOfText("");
+        mainDocumentPart.addParagraphOfText("");
+        mainDocumentPart.addParagraphOfText("");
+
+        File image = new File("src/main/resources/images/documentovisco.png");
+        byte[] fileContent = Files.readAllBytes(image.toPath());
+        BinaryPartAbstractImage imagePart = BinaryPartAbstractImage
+                .createImagePart(wordPackage, fileContent);
+        Inline inline = imagePart.createImageInline("", "", 1, 2,
+                wordPackage.getDocumentModel().getSections().get(0).getPageDimensions().getWritableWidthTwips() / 3,
+                false);
+        P Imageparagraph = addImageToParagraph(inline);
+        mainDocumentPart.getContent().add(Imageparagraph);
+
+        mainDocumentPart.addParagraphOfText("Documentovisco ©");
+
+
+        File exportFile = new File(reportPath + reportFileName);
+        wordPackage.save(exportFile);
+
+//        new File(reportPath + reportFileName).delete();
+        return reportFileName;
+    }
+
+    private static void addCellContent(Tbl table, int row, int col, String value) {
+        ObjectFactory factory = Context.getWmlObjectFactory();
+        P p = factory.createP();
+        R r = factory.createR();
+        Text t = factory.createText();
+        t.setValue(value);
+        r.getContent().add(t);
+        p.getContent().add(r);
+
+        ((Tc)((Tr)table.getContent().get(row)).getContent().get(col)).getContent().add(p);
+    }
+
+    private static P addImageToParagraph(Inline inline) {
+        ObjectFactory factory = new ObjectFactory();
+        P p = factory.createP();
+        R r = factory.createR();
+        p.getContent().add(r);
+        Drawing drawing = factory.createDrawing();
+        r.getContent().add(drawing);
+        drawing.getAnchorOrInline().add(inline);
+        return p;
     }
 }
