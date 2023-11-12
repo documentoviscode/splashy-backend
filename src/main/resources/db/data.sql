3ff10538f1633ae7c1a4b80c12c5ed3e2b87fbd2
szymexus <62517155+SzymBrodz@users.noreply.github.com>
2023-11-12 18:32:01 +0100

    readjust data & comment bootstrap duplicate

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -18,10 +18,10 @@ VALUES  (1,0,'<GLinkDrive1>','2023-10-29'),
         (3,0,'<GLinkDrive3>','2023-10-27');*/
 
 INSERT INTO Partnership_Contracts(id, type, GDrive_Link, creation_date,start_date,end_date,rate,donation_percentage, user_id)
-VALUES  (4, 0,'<GLinkDrive4>','2023-08-01','2023-08-01','2023-09-30',1.0,69, 2), --Alicja first contract
+VALUES  (4, 0,'<GLinkDrive4>','2023-06-01','2023-06-01','2023-09-30',1.0,69, 2), --Alicja first contract
         (5, 0,'<GLinkDrive5>','2023-10-01','2023-10-01','2023-11-30',1.2,80, 2), --Alicja second contract
-        (6, 0,'<GLinkDrive6>','2023-09-01','2023-09-01','2023-11-30',0.9,10, 3), --Igor first contract
-        (7, 0,'<GLinkDrive7>','2023-09-01','2023-09-01','2023-11-30',1.1,99, 4); --Maja first contract
+        (6, 0,'<GLinkDrive6>','2023-06-01','2023-06-01','2023-11-30',0.9,10, 3), --Igor first contract
+        (7, 0,'<GLinkDrive7>','2023-06-01','2023-06-01','2023-11-30',1.1,99, 4); --Maja first contract
 
 /*
 INSERT INTO Additional_Packages(id, type, GDrive_Link, creation_date,package_type,price, user_id)
@@ -34,11 +34,18 @@ INSERT INTO Subscriptions(id, type, GDrive_Link, creation_date,start_date,period
 VALUES  (10,2,'<GLinkDrive10>','2023-10-01','2023-10-01',1,69.99, 5); --Ernest subscription
 
 INSERT INTO Monthly_Reports(id, type, GDrive_Link, creation_date,start_date,end_date,viewers,hours_watched,donations, user_id)
-VALUES  (13,2,'<GLinkDrive13>','2023-08-01','2023-08-01','2023-08-31',1111,454545.5,1232, 2), --Alicja August monthly report
-        (14,3,'<GLinkDrive14>','2023-09-01','2023-09-01','2023-09-30',2342,985945,2342, 2), --Alicja September monthly report
-        (15,3,'<GLinkDrive15>','2023-10-01','2023-10-01','2023-10-31',10003,123333.3,3455, 2), --Alicja October monthly report
-        (16,3,'<GLinkDrive16>','2023-11-01','2023-11-01','2023-11-30',20332,256032,4500, 2), --Alicja November monthly report
-        (17,3,'<GLinkDrive17>','2023-09-01','2023-09-01','2023-09-31',20332,502532,9504, 3), --Igor September monthly report
-        (18,3,'<GLinkDrive18>','2023-10-01','2023-10-01','2023-10-30',35322,1232522.3,1002, 3), --Igor October monthly report
-        (19,3,'<GLinkDrive19>','2023-09-01','2023-09-01','2023-09-31',532,64321.3,321, 4), --Maja September monthly report
-        (20,3,'<GLinkDrive20>','2023-10-01','2023-10-01','2023-10-30',513,35123,235, 4); --Maja October monthly report
+VALUES  (13,2,'<GLinkDrive13>','2023-06-01','2023-06-01','2023-06-30',231,3513,132, 2), --Alicja June monthly report
+        (14,3,'<GLinkDrive14>','2023-07-01','2023-07-01','2023-07-31',542,23521,235, 2), --Alicja July monthly report
+        (15,2,'<GLinkDrive15>','2023-08-01','2023-08-01','2023-08-31',1111,454545.5,1232, 2), --Alicja August monthly report
+        (16,3,'<GLinkDrive16>','2023-09-01','2023-09-01','2023-09-30',2342,985945,2342, 2), --Alicja September monthly report
+        (17,3,'<GLinkDrive17>','2023-10-01','2023-10-01','2023-10-31',10003,123333.3,3455, 2), --Alicja October monthly report
+        (18,3,'<GLinkDrive18>','2023-06-01','2023-06-01','2023-06-30',5303,351513,2344, 3), --Igor June monthly report
+        (19,3,'<GLinkDrive19>','2023-07-01','2023-07-01','2023-07-31',2321,531291,3421, 3), --Igor July monthly report
+        (20,3,'<GLinkDrive20>','2023-08-01','2023-08-01','2023-08-30',12042,34141,5432, 3), --Igor August monthly report
+        (21,3,'<GLinkDrive21>','2023-09-01','2023-09-01','2023-09-30',20332,502532,9504, 3), --Igor September monthly report
+        (22,3,'<GLinkDrive22>','2023-10-01','2023-10-01','2023-10-31',35322,1232522.3,1002, 3), --Igor October monthly report
+        (23,3,'<GLinkDrive23>','2023-06-01','2023-06-01','2023-06-30',152,6432.3,321, 4), --Maja June monthly report
+        (24,3,'<GLinkDrive24>','2023-07-01','2023-07-01','2023-07-31',346,3531.2,142, 4), --Maja July monthly report
+        (25,3,'<GLinkDrive25>','2023-08-01','2023-08-01','2023-08-30',421,35123,235, 4), --Maja August monthly report
+        (26,3,'<GLinkDrive26>','2023-09-01','2023-09-01','2023-09-30',532,64321.3,321, 4), --Maja September monthly report
+        (27,3,'<GLinkDrive27>','2023-10-01','2023-10-01','2023-10-31',513,35123,235, 4); --Maja October monthly report
